7547e0ed7debf713f8f8804b98398536efd16887
Moussa Sow <ousseynousow180@gmail.com>
2026-04-26 19:57:33 +0000

    modified the tab icons

--- blog-app/app/(tabs)/_layout.jsx
+++ blog-app/app/(tabs)/_layout.jsx
@@ -1,10 +1,11 @@
 import { Tabs } from 'expo-router';
+import Feather from '@expo/vector-icons/Feather';
 
 export default function TabLayout() {
     return(
         <Tabs>
-            <Tabs.Screen name="home"/>
-            <Tabs.Screen name="create-post"/>
+            <Tabs.Screen name="home" options={{headerShown: false, title: "Home", tabBarIcon: ()=> <Feather name="home" size={24} />}}/>
+            <Tabs.Screen name="create-post" options={{headerShown: false, title:"Create Post", tabBarIcon: ()=> <Feather name="plus" size={24} />}}/>
         </Tabs>
     )
 }
