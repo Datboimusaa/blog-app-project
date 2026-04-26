8337ba4e08c081efc7cc57a5a902c56d716740a2
Moussa Sow <ousseynousow180@gmail.com>
2026-04-26 15:43:00 +0000

    added the mobile version

--- blog-api-frontend/src/contexts/CommentContext.jsx
+++ blog-api-frontend/src/contexts/CommentContext.jsx
@@ -4,8 +4,17 @@ import API from '../services/API'
 export const CommentContext = useContext();
 
 export const CommentProvider = ({children}) => {
+
+    const [comments, setComments] = useState([])
+
+    const getComments = async(id) => {
+        const res = await API.get(`/comments/post/${id}`);
+        setComments(res.data)
+    }
+
+    
     return (
-        <CommentContext.Provider value={}>
+        <CommentContext.Provider value={{comments, getComments}}>
             {children}
         </CommentContext.Provider>
     )
